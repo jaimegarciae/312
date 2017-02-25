a83c3581cc7f16b83c7fac68329eb7a54a2eddeb
jaimegarciae <jaimegarcia@utexas.edu>
2017-02-25 17:37:57 -0600

    Uploads changes to CRM project

---
+++ Projects/4. CRM/cmake-build-debug/CMakeFiles/4__CRM.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/4__CRM.dir/main.cpp.o"
+  "CMakeFiles/4__CRM.dir/MyString.cpp.o"
+  "CMakeFiles/4__CRM.dir/Project4.cpp.o"
+  "4__CRM.pdb"
+  "4__CRM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4__CRM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
