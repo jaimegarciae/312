8f225132f75346a0b78e46a11f47c80ce3028e40
jaimegarcaie <jaimegarcia@utexas.edu>
2017-02-25 18:35:40 -0600

    Fixes inventory name duplication issue

--- Projects/4. CRM/Project4.cpp
+++ Projects/4. CRM/Project4.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * EE312 Simple CRM Project
  *
  * Jaime Eugenio Garcia
@@ -14,188 +14,188 @@
 Customer customers[MAX_CUSTOMERS];
 int num_customers = 0;
 // Customer type variable used to store the inventory
-Customer inventory;
+Customer stock;
 
 void reset(void) {
-	inventory.bottles = 0;
-	inventory.diapers = 0;
-	inventory.rattles = 0;
-	num_customers = 0;
+    stock.bottles = 0;
+    stock.diapers = 0;
+    stock.rattles = 0;
+    num_customers = 0;
 }
 
 int checkBottles(String *type){
-	int isEqual;
-	String bottles = StringCreate("Bottles");
-	if(StringIsEqualTo(type, &bottles)){
-		isEqual = 1;
-	} else{
-		isEqual = 0;
-	}
-	StringDestroy(&bottles);
-	return isEqual;
+    int isEqual;
+    String bottles = StringCreate("Bottles");
+    if(StringIsEqualTo(type, &bottles)){
+        isEqual = 1;
+    } else{
+        isEqual = 0;
+    }
+    StringDestroy(&bottles);
+    return isEqual;
 }
 
 int checkDiapers(String *type){
-	int isEqual;
-	String diapers = StringCreate("Diapers");
-	if(StringIsEqualTo(type, &diapers)){
-		isEqual = 1;
-	} else{
-		isEqual = 0;
-	}
-	StringDestroy(&diapers);
-	return isEqual;
+    int isEqual;
+    String diapers = StringCreate("Diapers");
+    if(StringIsEqualTo(type, &diapers)){
+        isEqual = 1;
+    } else{
+        isEqual = 0;
+    }
+    StringDestroy(&diapers);
+    return isEqual;
 }
 
 int checkRattles(String *type){
-	int isEqual;
-	String rattles = StringCreate("Rattles");
-	if(StringIsEqualTo(type, &rattles)){
-		isEqual = 1;
-	} else{
-		isEqual = 0;
-	}
-	StringDestroy(&rattles);
-	return isEqual;
+    int isEqual;
+    String rattles = StringCreate("Rattles");
+    if(StringIsEqualTo(type, &rattles)){
+        isEqual = 1;
+    } else{
+        isEqual = 0;
+    }
+    StringDestroy(&rattles);
+    return isEqual;
 }
 
 void sorryMessage(int *index, String *type, int *inStock){
-	printf("Sorry ");
-	StringPrint(&customers[*index].name);
-	printf(", we only have %d ", *inStock);
-	StringPrint(type);
-	printf("\n");
-	return;
+    printf("Sorry ");
+    StringPrint(&customers[*index].name);
+    printf(", we only have %d ", *inStock);
+    StringPrint(type);
+    printf("\n");
+    return;
 }
 
 void mostBottles(){
-	int max = 0;
-	int index;
-	for(int i = 0; i < num_customers; i++){
-		if(max < customers[i].bottles){
-			max = customers[i].bottles;
-			index = i;
-		}
-	}
-	if(max != 0){
-		printf("no one has purchased any Bottles");
-	} else{
-		StringPrint(&customers[index].name);
-		printf(" has purchased the most Bottles (%d)", customers[index].bottles);
+    int max = 0;
+    int index;
+    for(int i = 0; i < num_customers; i++){
+        if(max < customers[i].bottles){
+            max = customers[i].bottles;
+            index = i;
+        }
+    }
+    if(max != 0){
+        printf("no one has purchased any Bottles");
+    } else{
+        StringPrint(&customers[index].name);
+        printf(" has purchased the most Bottles (%d)", customers[index].bottles);
 
-	}
+    }
 }
 
 void mostDiapers(){
-	int max = 0;
-	int index;
-	for(int i = 0; i < num_customers; i++){
-		if(max < customers[i].diapers){
-			max = customers[i].diapers;
-			index = i;
-		}
-	}
-	if(max != 0){
-		printf("no one has purchased any Diapers");
-	} else{
-		StringPrint(&customers[index].name);
-		printf(" has purchased the most Diapers (%d)", customers[index].diapers);
+    int max = 0;
+    int index;
+    for(int i = 0; i < num_customers; i++){
+        if(max < customers[i].diapers){
+            max = customers[i].diapers;
+            index = i;
+        }
+    }
+    if(max != 0){
+        printf("no one has purchased any Diapers");
+    } else{
+        StringPrint(&customers[index].name);
+        printf(" has purchased the most Diapers (%d)", customers[index].diapers);
 
-	}
+    }
 }
 
 void mostRattles(){
-	int max = 0;
-	int index;
-	for(int i = 0; i < num_customers; i++){
-		if(max < customers[i].rattles){
-			max = customers[i].rattles;
-			index = i;
-		}
-	}
-	if(max != 0){
-		printf("no one has purchased any Rattles");
-	} else{
-		StringPrint(&customers[index].name);
-		printf(" has purchased the most Rattles (%d)", customers[index].rattles);
+    int max = 0;
+    int index;
+    for(int i = 0; i < num_customers; i++){
+        if(max < customers[i].rattles){
+            max = customers[i].rattles;
+            index = i;
+        }
+    }
+    if(max != 0){
+        printf("no one has purchased any Rattles");
+    } else{
+        StringPrint(&customers[index].name);
+        printf(" has purchased the most Rattles (%d)", customers[index].rattles);
 
-	}
+    }
 }
 
 void processSummarize() {
-	printf("There are %d Bottles, %d Diapers and %d Rattles in inventory\n", inventory.bottles, inventory.diapers, inventory.rattles);
-	printf("we have had a total of %d different customers\n", num_customers);
-	mostBottles();
-	mostDiapers();
-	mostRattles();
-	return;
+    printf("There are %d Bottles, %d Diapers and %d Rattles in inventory\n", stock.bottles, stock.diapers, stock.rattles);
+    printf("we have had a total of %d different customers\n", num_customers);
+    mostBottles();
+    mostDiapers();
+    mostRattles();
+    return;
 }
 
 void makePurchase(int index, String *type, int *amount){
-	if(checkBottles(type)){
-		if(inventory.bottles < *amount){
-			sorryMessage(&index, type, &inventory.bottles);
-		} else{
-			customers[index].bottles += *amount;
-			inventory.bottles -= *amount;
-		}
-	} else if(checkDiapers(type)){
-		if(inventory.bottles < *amount){
-			sorryMessage(&index, type, &inventory.diapers);
-		} else{
-			customers[index].diapers += *amount;
-			inventory.diapers -= *amount;
-		}
-	} else if(checkRattles(type)){
-		if(inventory.bottles < *amount){
-			sorryMessage(&index, type, &inventory.rattles);
-		} else{
-			customers[index].rattles += *amount;
-			inventory.rattles -= *amount;
-		}
-	}
-	return;
+    if(checkBottles(type)){
+        if(stock.bottles < *amount){
+            sorryMessage(&index, type, &stock.bottles);
+        } else{
+            customers[index].bottles += *amount;
+            stock.bottles -= *amount;
+        }
+    } else if(checkDiapers(type)){
+        if(stock.bottles < *amount){
+            sorryMessage(&index, type, &stock.diapers);
+        } else{
+            customers[index].diapers += *amount;
+            stock.diapers -= *amount;
+        }
+    } else if(checkRattles(type)){
+        if(stock.bottles < *amount){
+            sorryMessage(&index, type, &stock.rattles);
+        } else{
+            customers[index].rattles += *amount;
+            stock.rattles -= *amount;
+        }
+    }
+    return;
 }
 
 int customerSearch(String *name){
-	for(int i = 0; i < num_customers; i++){
-		if(StringIsEqualTo(name, &customers[i].name)){
-			return i;
-		}
-	}
-	customers[num_customers].name = *name;
-	num_customers++;
-	return (num_customers - 1);
+    for(int i = 0; i < num_customers; i++){
+        if(StringIsEqualTo(name, &customers[i].name)){
+            return i;
+        }
+    }
+    customers[num_customers].name = *name;
+    num_customers++;
+    return (num_customers - 1);
 }
 
 void processPurchase() {
-	String name, type;
-	int amount;
-	readString(&name);
-	readString(&type);
-	readNum(&amount);
-	makePurchase(customerSearch(&name), &type, &amount);
-	StringDestroy(&name);
-	StringDestroy(&type);
+    String name, type;
+    int amount;
+    readString(&name);
+    readString(&type);
+    readNum(&amount);
+    makePurchase(customerSearch(&name), &type, &amount);
+    StringDestroy(&name);
+    StringDestroy(&type);
 }
 
 void addToInventory(String *type, int *amount){
-	if(checkBottles(type)){
-		inventory.bottles += *amount;
-	} else if(checkDiapers(type)){
-		inventory.diapers += *amount;
-	} else if(checkRattles(type)){
-		inventory.rattles += *amount;
-	}
-	return;
+    if(checkBottles(type)){
+        stock.bottles += *amount;
+    } else if(checkDiapers(type)){
+        stock.diapers += *amount;
+    } else if(checkRattles(type)){
+        stock.rattles += *amount;
+    }
+    return;
 }
 
 void processInventory() {
-	String type;
-	int amount;
-	readString(&type);
-	readNum(&amount);
-	addToInventory(&type, &amount);
-	StringDestroy(&type);
-	return;
+    String type;
+    int amount;
+    readString(&type);
+    readNum(&amount);
+    addToInventory(&type, &amount);
+    StringDestroy(&type);
+    return;
 }
