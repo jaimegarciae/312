4a72bb575aa4b3e370e44de1ec89677fc75c2117
jaimegarcaie <jaimegarcia@utexas.edu>
2017-04-20 17:32:59 -0500

    Tests more cases

--- Projects/8. Interpreter/main.cpp
+++ Projects/8. Interpreter/main.cpp
@@ -1,9 +1,14 @@
+/*
+	Jaime Eugenio Garcia
+	JEG3954
+*/
+
 #include <assert.h>
 #include "Parse.h"
 #include "handler.h"
 
 int main(){
-	set_input("test4.blip");
+	set_input("test_grader.blip");
     run();
 	return 0;
 }
